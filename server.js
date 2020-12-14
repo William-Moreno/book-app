1ea085b24862da14c0922e0dd81f50219cb61068
William-Moreno <intrcptr@comcast.net>
2020-12-14 14:10:10 -0800

    setting up server

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const pg = require('pg');
+const superagent = require('superagent');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
+
+
+
+// error handling and start server
+app.use('*', (request, response) => {
+  response.status(404).send('The route you are looking for has been disconnected. Please try another.');
+});
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
