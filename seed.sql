c5fb278ce349f16eb12efe47f754ac6f2c51c576
William-Moreno <intrcptr@comcast.net>
2020-12-15 16:13:44 -0800

    database set-up and rendering to index.ejs

---
+++ seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (author, title, isbn, image_url, description) VALUES ('J.K. Rowling','Harry Potter','xxxxxxxxxxxxxx','https://i.imgur.com/J5LVHEL.jpg', 'A very interesting read.');
+
+INSERT INTO book (author, title, isbn, image_url, description) VALUES ('J.K. Rowling','Harry Potter 2','xxxxxxxxxxxxxx','https://i.imgur.com/J5LVHEL.jpg', 'A very interesting read.');
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', error => console.error(error));
 
 
 
@@ -24,7 +25,12 @@ app.post('/search', getBooks);
 
 
 function getHome(req, res){
-  res.render('pages/index.ejs');
+  client.query('SELECT * FROM book')
+    .then(result => {
+      const books = result.rows;
+      res.render('pages/index.ejs', {booklist: books});
+
+    });
 }
 
 function getHello(req, res){
@@ -42,7 +48,7 @@ function throwError(req, res){
 function getBooks(req, res){
   let searchString = req.body.query;
   let searchType = req.body.searchtype;
-  let url=`https://www.googleapis.com/books/v1/volumes?q=+${searchType}:${searchString}`;
+  let url=`https://www.googleapis.com/books/v1/volumes?q=+in${searchType}:${searchString}`;
   superagent.get(url).then(returnData => {
     const bookData = (returnData.body.items);
     const bookArray = bookData.map(function(book){
@@ -63,6 +69,7 @@ function BookData(book){
   this.author = book.volumeInfo.authors || 'Unlisted';
   this.description = book.volumeInfo.description || 'Not Available';
   this.image_url = book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail || `https://i.imgur.com/J5LVHEL.jpg`;
+  this.isbn = book.voulmeInfo.industryIdentifiers[0];
 }
 
 
@@ -71,4 +78,7 @@ app.use('*', (request, response) => {
   response.status(404).send('The route you are looking for has been disconnected. Please try another.');
 });
 
-app.listen(PORT, () => console.log(`Running on: ${PORT}`));
+
+client.connect().then(() => {
+  app.listen(PORT, () => console.log(`Running on: ${PORT}`));
+});
