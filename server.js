9de2ab6bd32a80eb936654408a9ef0ce85329080
William-Moreno <intrcptr@comcast.net>
2020-12-17 12:00:36 -0800

    changed redirect and changed size of images on view details page

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ function getBooks(req, res){
   }).catch(() => throwError);
 }
 
-function getDetails(req, res, form){
+function getDetails(req, res){
   client.query('SELECT * FROM book WHERE id=$1', [req.params.id])
     .then(result => {
       const bookdetail = result.rows[0];
@@ -99,7 +99,7 @@ function getDetails(req, res, form){
 function saveBookData(req, res){
   const chosenBook = (req.body);
   client.query('INSERT INTO book (author, title, image_url, isbn, description, categories) VALUES($1, $2, $3, $4, $5, $6)', [chosenBook.author, chosenBook.title, chosenBook.image_url, chosenBook.isbn, chosenBook.description, chosenBook.categories]).then(() => {
-    res.redirect('pages/books/show.ejs', {book: chosenBook});
+    res.redirect(`/`);
   });
 }
 
