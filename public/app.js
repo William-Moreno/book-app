b6c193150fea06f9f562f924b5af85630873f78b
WondwosenTsige <wondneh@gmail.com>
2020-12-17 15:46:37 -0800

    form ipmlemented ob search result page

--- public/app.js
+++ public/app.js
@@ -3,9 +3,15 @@
 const button = document.getElementById('show-form');
 const form = document.getElementById('update-form');
 const box = document.getElementById('btn-box');
+const book = document.getElementById('book-selector');
+const select = document.getElementById('select-book');
 
 button.addEventListener('click', () =>{
   form.style.display = 'block';
   box.style.display = 'none';
 });
 
+select.addEventListener('click', () =>{
+  book.style.display = 'block';
+  
+});
