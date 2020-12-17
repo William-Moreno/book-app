ed2f12085e565022e9c0b0014e2f7ab391f18dac
William-Moreno <intrcptr@comcast.net>
2020-12-16 22:56:13 -0800

    styling update form

--- public/app.js
+++ public/app.js
@@ -1,8 +1,11 @@
 'use strict';
 
-const button = document.getElementById('show-form')
-const form = document.getElementById('update-form')
+const button = document.getElementById('show-form');
+const form = document.getElementById('update-form');
+const box = document.getElementById('btn-box');
 
 button.addEventListener('click', () =>{
-    form.style.display='block'
-})
+  form.style.display = 'block';
+  box.style.display = 'none';
+});
+
