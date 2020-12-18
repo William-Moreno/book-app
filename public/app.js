f78b34efe844182fac5865d676b81e1464422b41
William-Moreno <intrcptr@comcast.net>
2020-12-18 00:12:04 -0800

    styled nav buttons

--- public/app.js
+++ public/app.js
@@ -5,13 +5,26 @@ const form = document.getElementById('update-form');
 const box = document.getElementById('btn-box');
 const book = document.getElementById('book-selector');
 const select = document.getElementById('select-book');
+const navOpen = document.getElementById('nav-open');
+const navClose = document.getElementById('nav-close');
+const navbar = document.getElementById('drop-down');
 
-button.addEventListener('click', () =>{
+
+button.addEventListener('click', () => {
   form.style.display = 'block';
   box.style.display = 'none';
 });
 
-select.addEventListener('click', () =>{
+select.addEventListener('click', () => {
   book.style.display = 'block';
-  
+});
+
+navOpen.addEventListener('click', (e) => {
+  navbar.className = navbar.className? 'retracted' : '';
+  e.preventDefault();
+});
+
+navClose.addEventListener('click', (e) => {
+  navbar.className = navbar.className? '' : 'retracted';
+  e.preventDefault();
 });
